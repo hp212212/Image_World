c3549a82eec3ce694b393ab317f424012e13b7e0
Hitesh <hp212212212@gmail.com>
2023-04-16 13:03:49 -0600

    next - 13

--- src/Components/js/Service.js
+++ src/Components/js/Service.js
@@ -1,6 +1,6 @@
 export const DefultImages = () => {
     let kaka = [
-        "https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MzYyMTJ8MXwxfHNlYXJjaHwxfHxuYXR1cmV8ZW58MHx8fHwxNjgxNTMzNzIw&ixlib=rb-4.0.3&q=80&w=1080"
+        "https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=Mnw0MzYyMTJ8MXwxfHNlYXJjaHwxfHxuYXR1cmV8ZW58MHx8fHwxNjgxNTMzNzIw&ixlib=rb-4.0.3&fm=webp&q=80&w=200"
 
     ]
     return kaka;
