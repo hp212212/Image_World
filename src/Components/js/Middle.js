efbc555c3b2d986fc96093ae2b0a6fbbbe0eb30d
Hitesh <hp212212212@gmail.com>
2023-04-16 14:36:58 -0600

    next - 18

--- src/Components/js/Middle.js
+++ src/Components/js/Middle.js
@@ -10,9 +10,9 @@ import { DefultImages } from './Service'
 
 export default function Middle() {
   let [Pages, setPages] = useState([])
-  // let DefultImg = DefultImages()
+  let DefultImg = DefultImages()
   const [Input, setInput] = useState('nature')
-  const [AllImages, setAllImages] = useState([])
+  const [AllImages, setAllImages] = useState(DefultImg)
   let [Current, setCurrent] = useState(1)
   let [MaxPageLimit, setMaxPageLimit] = useState(3)
   let [MinPageLimit, setMinPageLimit] = useState(0)
@@ -20,7 +20,7 @@ export default function Middle() {
 
   const MainFetch = async () => {
     let TempImg = []
-    await  fetch(`https://api.unsplash.com/search/photos?client_id=Bb-6szc-iyrTPIG_IFgEl2Rt3-HiUOLaOVA0bfbKJQU&page=${Current}&query=${Input}`)
+    await fetch(`https://api.unsplash.com/search/photos?client_id=Bb-6szc-iyrTPIG_IFgEl2Rt3-HiUOLaOVA0bfbKJQU&page=${Current}&query=${Input}`)
       .then(response => response.json())
       .then(data => {
         console.log(data)
